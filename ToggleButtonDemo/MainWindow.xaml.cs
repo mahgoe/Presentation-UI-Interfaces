6ce278f9b4094beb2a925bc6efefb694214d1b48
mahgoe <139371012+mahgoe@users.noreply.github.com>
2023-11-28 13:07:08 +0100

    init togglebutton

---
+++ ToggleButtonDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace ToggleButtonDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// If togglebutton active, change background color
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = sender as ToggleButton;
+            if (toggleButton.IsChecked == true)
+            {
+                this.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                this.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        /// <summary>
+        /// If toggle visible/invisible the box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void VisibilityToggle_Click(object sender, RoutedEventArgs e)
+        {
+            toggleTextBlock.Visibility = visibilityToggle.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
